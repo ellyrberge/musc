f76c6035cfbfa0f9a7df7abc60dc71091ef59990
eberge <eberge@eberges-MacBook-Pro.local>
2020-10-30 17:29:18 -0500

    routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 
 function App() {
   return (
-    <div>
-    </div>
+    <Router>
+      <Switch>
+          <Route path="/" exact={true}></Route>
+          <Route path="/playlist/:id/songs" exact={true}></Route>
+          <Route path="/search/:id" exact={true}></Route>
+          <Route path="/search/:id/:song_id" exact={true}></Route>
+      </Switch>
+    </Router>
+   
   );
 }
 
