07c553a3542f1ff9567c71fee4649d09b032fd76
eberge <eberge@eberges-MacBook-Pro.local>
2020-11-17 15:43:36 -0600

    fix call

--- src/music-api.js
+++ src/music-api.js
@@ -1,12 +1,12 @@
 export function fetchChart(){
-    return fetch('http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=52440ad91751e6bd7f5543a3fa845a5e&format=json')
+    return fetch('https://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=52440ad91751e6bd7f5543a3fa845a5e&format=json')
     .then((response) => {
          return response.json();
     })
 }
 
 export function fetchSearch(term){
-    return fetch('http://ws.audioscrobbler.com/2.0/?method=track.search&track='+term+'&api_key=52440ad91751e6bd7f5543a3fa845a5e&format=json')
+    return fetch('https://ws.audioscrobbler.com/2.0/?method=track.search&track='+term+'&api_key=52440ad91751e6bd7f5543a3fa845a5e&format=json')
     .then((response) => {
         return response.json();
     }).catch((error) => {
@@ -16,7 +16,7 @@ export function fetchSearch(term){
 }
 
 export async function fetchSong(title, artist){
-    return fetch('http://ws.audioscrobbler.com/2.0/?method=track.getInfo&track='+title+'&artist='+artist+'&api_key=52440ad91751e6bd7f5543a3fa845a5e&format=json')
+    return fetch('https://ws.audioscrobbler.com/2.0/?method=track.getInfo&track='+title+'&artist='+artist+'&api_key=52440ad91751e6bd7f5543a3fa845a5e&format=json')
     .then((response) => {
         return response.json();
     });
